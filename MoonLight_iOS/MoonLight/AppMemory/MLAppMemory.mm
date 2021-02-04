f8acb916c8f344128ad7da1d5399d1dea291d7a9
DevDragonLi <Dragonli_52171@163.com>
2021-02-04 10:18:02 +0800

    getAppMemory function adapter iOS 13+ API

--- MoonLight_iOS/MoonLight/AppMemory/MLAppMemory.mm
+++ MoonLight_iOS/MoonLight/AppMemory/MLAppMemory.mm
@@ -8,19 +8,26 @@
 #import "MLAppMemory.h"
 #if TARGET_OS_IPHONE
 #include <mach/mach.h>
+#import <os/proc.h>
 #endif
 
 @implementation MLAppMemory
 
-+ (float)getAppMemory{
++ (float)getAppMemory {
+    
     NSUInteger memoryUsageInByte = 0;
-    task_vm_info_data_t vmInfo;
-    mach_msg_type_number_t count = TASK_VM_INFO_COUNT;
-    kern_return_t kernelReturn = task_info(mach_task_self(), TASK_VM_INFO, (task_info_t) &vmInfo, &count);
-    if(kernelReturn == KERN_SUCCESS) {
-        memoryUsageInByte = (NSUInteger) vmInfo.phys_footprint;
+    
+    if (@available(iOS 13.0, *)) {
+        memoryUsageInByte = os_proc_available_memory();
     } else {
-        return -1;
+        task_vm_info_data_t vmInfo;
+        mach_msg_type_number_t count = TASK_VM_INFO_COUNT;
+        kern_return_t kernelReturn = task_info(mach_task_self(), TASK_VM_INFO, (task_info_t) &vmInfo, &count);
+        if(kernelReturn == KERN_SUCCESS) {
+            memoryUsageInByte = (NSUInteger) vmInfo.phys_footprint;
+        } else {
+            return -1;
+        }
     }
     return memoryUsageInByte/1024.0/1024.0;
 }
